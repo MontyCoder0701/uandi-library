f30d620d38813651945144762d16c7460e8a78a0
Soo <soojlee0106@naver.com>
2024-02-03 16:46:11 +0900

    feat: create cards with radio

--- projects/uandi/src/lib/atom/card/card.component.ts
+++ projects/uandi/src/lib/atom/card/card.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'uai-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
   standalone: true,
+  imports: [NgIf],
 })
 export class CardComponent {
   @Input() title = '';
   @Input() text = '';
+  @Input() hasRadio = false;
 }
