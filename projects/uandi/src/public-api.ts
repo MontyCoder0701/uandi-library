273c2180f7d23f02bc5270c28104139b75b1c47c
Soo <soojlee0106@naver.com>
2024-02-03 18:28:36 +0900

    refactor: structure

--- projects/uandi/src/public-api.ts
+++ projects/uandi/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of uandi
  */
 
-export * from './lib/atom/button/button.component';
-export * from './lib/atom/input/input.component';
-export * from './lib/atom/card/card.component';
+export * from './lib/atoms/button/button.component';
+export * from './lib/atoms/input/input.component';
+export * from './lib/molecules/card/card.component';
